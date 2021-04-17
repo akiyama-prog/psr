3fe2ef6906c5c55d65a9bcf94c8ac1c51a48a2ce
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-04-17 21:50:27 +0900

    composer require nyholm/psr7

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,47 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit4506afc9ff072c2291b9e72e7e650932
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Psr\\Http\\Message\\' => 17,
+        ),
+        'N' => 
+        array (
+            'Nyholm\\Psr7\\' => 12,
+            'Nyholm\\Psr7Server\\' => 18,
+        ),
+        'H' => 
+        array (
+            'Http\\Message\\' => 13,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'Nyholm\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nyholm/psr7/src',
+        ),
+        'Nyholm\\Psr7Server\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nyholm/psr7-server/src',
+        ),
+        'Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-http/message-factory/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit4506afc9ff072c2291b9e72e7e650932::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit4506afc9ff072c2291b9e72e7e650932::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
