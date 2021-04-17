c7427234a81d48a1b7642481ca12ca1e38259671
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-04-17 22:36:48 +0900

    psr12

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
+
 require __DIR__ . '/../vendor/autoload.php';
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops = new \Whoops\Run();
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 $whoops->register();
 
 $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
